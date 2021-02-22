1860e0752fd15debcfd5450f084958b8fc13a2df
Kélison Rosendo da Rocha <kelisonrosendo@gmail.com>
2021-02-22 19:42:16 -0300

    experience bar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import { ExperienceBar } from './components/ExperienceBar';
+import './styles/global.css';
+
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <div className="container">
+      <ExperienceBar/>
+    </div>
   );
 }
 
